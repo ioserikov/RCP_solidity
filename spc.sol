868b38015f61a1bb11dc2fa4522a51c24f0fe0e4
iserikov <igor@serikov.ru>
2019-06-27 19:11:59 +0300

    first task

--- spc.sol
+++ spc.sol
@@ -1,150 +1,76 @@
+pragma solidity ^0.5.1;
+
+contract rpc_game{
+    address creator;
+    address public  player1;
+    address public  palyer2;
+    uint256 public stake; // in weis
+    bool public turn1done;
+    bool public turn2done;
+
+    enum resultenum {winfirst, winsecond, nowin }
+
+    enum turnenum {scissors, paper, rock}
+    
+    turnenum private  turn1value; 
+    turnenum private  turn2value; 
+    
+    constructor () public {
+            creator = msg.sender;
 
 
-contract rpsAdvanced
-{
-    mapping (string => mapping(string => int)) payoffMatrix;
-    address player1;
-    address player2;
-    bytes32 player1ChoiceHash;
-    bytes32 player2ChoiceHash;
-    string public player1Choice;
-    string public player2Choice;
-    uint firstRevealTime;
-
-    modifier notRegisteredYet()
-    {
-        if (msg.sender == player1 || msg.sender == player2)
-            throw;
-        else
-            _
     }
 
-    modifier isRegistered()
-    {
-        if (msg.sender != player1 && msg.sender != player2)
-            throw;
-        else
-            _
+
+
+    function Play()  public returns (resultenum win){
+                    
+           require(turn2done == true);
+            resultenum result = resultenum.nowin;
+            
+            
+            result = countresult(turn1value,turn2value);
+            
+            return result;
+
+   }
+
+
+     // internal result determining
+    function countresult(turnenum turn1, turnenum turn2) private returns (resultenum result){
+   
+    result = resultenum.nowin; 
+    if (turn1 == turn2)
+        { return result; }
+    if ((turn1 == turnenum.scissors && turn2 ==turnenum.paper) || (turn1 == turnenum.paper && turn2 == turnenum.rock) || (turn1 == turnenum.rock && turn2 == turnenum.scissors )) {
+            result = resultenum.winfirst;
+            return result;    
+    }else{
+       result = resultenum.winsecond;
+       return result;
     }
     
-    modifier sentEnoughCash(uint amount)
-    {
-        if (msg.value < amount)
-            throw;
-        else
-            _
-    }
+    }    
     
-    modifier validChoice(string choice)
-    {
-        // hack until we can use StringUtils.equal
-        if (sha3(choice) != sha3("rock") && sha3(choice) != sha3("paper") && sha3(choice) != sha3("scissors"))
-            throw;
-        else
-            _
+    function setStake(uint256 value) public {
+        require (msg.sender == creator);
+        stake = value;
     }
+        
+        
     
-    function rpsAdvanced() 
-    {   // constructor (spoiler alert: rename this if you rename the contract!)
-        payoffMatrix["rock"]["rock"] = 0;
-        payoffMatrix["rock"]["paper"] = 2;
-        payoffMatrix["rock"]["scissors"] = 1;
-        payoffMatrix["paper"]["rock"] = 1;
-        payoffMatrix["paper"]["paper"] = 0;
-        payoffMatrix["paper"]["scissors"] = 2;
-        payoffMatrix["scissors"]["rock"] = 2;
-        payoffMatrix["scissors"]["paper"] = 1;
-        payoffMatrix["scissors"]["scissors"] = 0;
-    }
     
-    function register()
-        sentEnoughCash(5)
-        notRegisteredYet
-    {
-        if (player1 == 0)
+    function MakeTurn(turnenum turn) public {
+        require (stake > 0);
+        if (turn1done == false) {
             player1 = msg.sender;
-        else if (player2 == 0)
-            player2 = msg.sender;
-    }
-
-    function play(string choice, string randStr) 
-        isRegistered
-        validChoice(choice)
-    {
-        if (msg.sender == player1)
-            player1ChoiceHash = sha3(sha3(choice) ^ sha3(randStr));
-        else if (msg.sender == player2)
-            player2ChoiceHash = sha3(sha3(choice) ^ sha3(randStr));
-    }
-    
-    function reveal(string choice, string randStr)
-        isRegistered
-        validChoice(choice)
-    {
-        // second player has 120 seconds after first player revealed
-        if (bytes(player1Choice).length == 0 && bytes(player2Choice).length == 0)
-            firstRevealTime == now;
-
-        // if hashed choice + randStr is matching the initial one, choice is stored
-        if (msg.sender == player1 && sha3(sha3(choice) ^ sha3(randStr)) == player1ChoiceHash)
-            player1Choice = choice;
-        if (msg.sender == player2 && sha3(sha3(choice) ^ sha3(randStr)) == player2ChoiceHash)
-            player2Choice = choice;
-    }
-    
-    function checkWinner()
-    {
-        if (bytes(player1Choice).length != 0 && bytes(player2Choice).length != 0)
-        {
-            // if both revealed, obtain winner in usual way
-            int winner = payoffMatrix[player1Choice][player2Choice];
-            if (winner == 1)
-                player1.send(this.balance);
-            else if (winner == 2)
-                player2.send(this.balance);
-            else
-            {
-                player1.send(this.balance/2);
-                player2.send(this.balance);
-            }
-
-            // unregister players and choices
-            player1Choice = "";
-            player2Choice = "";
-            player1 = 0;
-            player2 = 0;
-        }
-        else if (now > firstRevealTime + 120)
-        {
-            // if only one player revealed and time > start + timeout, winner is the one who revealed first
-            if (bytes(player1Choice).length != 0)
-                player1.send(this.balance);
-            else if (bytes(player2Choice).length != 0)
-                player2.send(this.balance);
+            turn1done = true;
+            turn1value = turn;
+            return;   
         }
         
     }
     
-    // HELPER FUNCTIONS
-    function getMyBalance () constant returns (uint amount)
-    {
-        return msg.sender.balance;
-    }
     
-    function getContractBalance () constant returns (uint amount)
-    {
-        return this.balance;
-    }
     
-    function AmIPlayer1() constant returns (bool x)
-    {
-        return msg.sender == player1;
-    }
-    
-    function AmIPlayer2() constant returns (bool x)
-    {
-        return msg.sender == player2;
-    }
-    
-    // \HELPER FUNCTIONS
 }
